ec1f8c9eb53344c1a6138e525c188a98df1ecfb4
sm4sh1k <v4vs@yandex.ru>
2015-02-01 13:27:10 +0700

    Added fb scripts

---
+++ servers/fb_backup_level0.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+::Script for making full Firebird database backup (level 0) via nbackup utility,
+:: archiving it with 7-zip and deleting old archives (95 days older).
+::The full script set consist of three similar scripts for making incremental
+:: everyday backups, weekly backups and full monthly backups.
+::Author: Valentin Vakhrushev, 2014
+
+SET nbackup="C:\Program Files (x86)\Firebird\Firebird_2_5\bin\nbackup.exe"
+SET sevenzip="C:\Program Files\7-Zip\7z.exe"
+SET database=X:\BASE\MAIN.FDB
+SET archivepath=Y:\BACKUP\
+SET dbuser=SYSDBA
+SET dbpass=masterkey
+
+SET year=%date:~-4%
+SET month=%date:~3,2%
+IF "%month:~0,1%" == " " SET month=0%month:~1,1%
+SET day=%date:~0,2%
+IF "%day:~0,1%" == " " SET day=0%day:~1,1%
+SET archivename=MAIN_0_%year%-%month%-%day%
+
+ECHO Creating database backup (level 0)...
+%nbackup% -U %dbuser% -P %dbpass% -B 0 %database% %archivepath%%archivename%.nbk
+IF %ERRORLEVEL% == 0 GOTO 7zip
+EXIT 1
+
+:7zip
+ECHO Archiving backup file...
+%sevenzip% a "%archivepath%%archivename%.7z" "%archivepath%%archivename%.nbk"
+IF %ERRORLEVEL% == 0 GOTO finish
+EXIT 1
+
+:finish
+ECHO Deleting original backup file...
+DEL /f /q "%archivepath%%archivename%.nbk"
+ECHO Deleting old archives (95 days older)...
+CD /D "%archivepath%"
+FORFILES /M MAIN_0_*-*-*.7z /D -95 /C "cmd /c if @isdir==FALSE del /f /q @file"
+EXIT 0
